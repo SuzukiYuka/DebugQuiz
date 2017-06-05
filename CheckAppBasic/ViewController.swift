af690cbd5006c7bf4fbfdb747e4b45029290326e
SUZUKIyuka <suzukiyuka1218@gmail.com>
2017-06-05 11:48:23 +0900

    first commit

--- CheckAppBasic/ViewController.swift
+++ CheckAppBasic/ViewController.swift
@@ -11,49 +11,70 @@ import UIKit
 class ViewController: UIViewController {
     
     var index: Int = 0
+    
     @IBOutlet var label: UILabel!
-    var appleArray: [String] = ["iPhone", "iPad", "Apple Watch"]
+    var nameArray: [String] = ["iPhone", "Android", "WebS", "WebD", "Game"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = appleArray[index]
+        
+        label.text = nameArray[index]
+        
+
     }
     
     @IBAction func next() {
         
-        if index < 2 {
+        if index < 10 {
+            
             index = index + 1
-            label.text = appleArray[index]
+            label.text = nameArray[index]
         }
     }
     
     @IBAction func mae() {
         
         if index > 0 {
+            
             index = index - 1
-            label.text = appleArray[index]
+            label.text = nameArray[index]
         }
     }
     
     @IBAction func first() {
         
         index = 0
-        label.text = appleArray[index]
+        label.text = nameArray[index]
     }
     
     @IBAction func second() {
         
         index = 1
-        label.text = appleArray[index]
+        label.text = nameArray[index]
     }
     
     @IBAction func third() {
         
         index = 2
-        label.text = appleArray[index]
+        label.text = nameArray[index]
     }
     
+    @IBAction func fourth() {
+        
+        index = 3
+        label.text = nameArray[index]
+    }
     
-
+    @IBAction func fifth() {
+        
+        index = 4
+        label.text = nameArray[index]
+    }
+    
+    @IBAction func sixth() {
+        
+        index = 5
+        label.text = nameArray[index]
+    }
 }
 
